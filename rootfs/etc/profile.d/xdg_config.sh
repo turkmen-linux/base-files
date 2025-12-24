f2079bbdadb966d2bf208fab0fc6de646351c89f
aliriza <aliriza.keskin@pardus.org.tr>
2025-12-24 10:32:25 +0300

    turkman -> turkmen

--- rootfs/etc/profile.d/xdg_config.sh
+++ rootfs/etc/profile.d/xdg_config.sh
@@ -5,5 +5,5 @@ fi
 if [ "${XDG_DATA_DIRS}" == "" ] ; then
     export XDG_DATA_DIRS=/usr/local/share:/usr/share
 fi
-export XDG_CONFIG_DIRS=/etc/xdg/turkman:${XDG_CONFIG_DIRS}
-export XDG_DATA_DIRS=/usr/share/turkman:${XDG_DATA_DIRS}
+export XDG_CONFIG_DIRS=/etc/xdg/turkmen:${XDG_CONFIG_DIRS}
+export XDG_DATA_DIRS=/usr/share/turkmen:${XDG_DATA_DIRS}
